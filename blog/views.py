eef755fc7b739267dc8ec2b8a7eda1280328cb3c
younghookim <younghoo88@gmail.com>
2016-04-27 00:24:15 +0900

    Add detail_view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+
+# view는 모델과 템플릿을 연결해주는 역할을 한다.
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    print(post.pk)
+    print(post.id)
+    return render(request, 'blog/post_detail.html', {'post': post})
